b0f78cad47a240a99336232a58f94ac29e357a85
d4niz <josedanielmorebolopez@gmail.com>
2024-06-15 20:50:58 +0200

    Generate Salt & Hash with Salt

--- cryptor.js
+++ cryptor.js
@@ -1,32 +1,46 @@
-import { pbkdf2, pbkdf2Sync } from 'crypto';
+import { pbkdf2, pbkdf2Sync, randomBytes } from "crypto";
 
 const cryptor = {
-  hash: (string, salt) => {
+  generateSalt: (length = 16) => {
+    return randomBytes(length).toString("hex");
+  },
+
+  hash: (string, salt, iterations = 10000, keyLength = 64, digest = "sha512") => {
     return new Promise((resolve, reject) => {
-      pbkdf2(string, salt.toString(), 10000, 64, 'sha512', (err, derivedKey) => {
+      pbkdf2(string, salt, iterations, keyLength, digest, (err, derivedKey) => {
         if (err) reject(err);
-        resolve(derivedKey.toString('hex'));
+        resolve(derivedKey.toString("hex"));
       });
     });
   },
 
-  compare: (string, hashedString, salt) => {
+  compare: (string, hashedString, salt, iterations = 10000, keyLength = 64, digest = "sha512") => {
     return new Promise((resolve, reject) => {
-      cryptor.hash(string, salt).then(hash => {
-        resolve(hash === hashedString);
-      }).catch(err => reject(err));
+      cryptor
+        .hash(string, salt, iterations, keyLength, digest)
+        .then((hash) => {
+          resolve(hash === hashedString);
+        })
+        .catch((err) => reject(err));
     });
   },
 
-  hashSync: (string, salt) => {
-    const derivedKey = pbkdf2Sync(string, salt.toString(), 10000, 64, 'sha512');
-    return derivedKey.toString('hex');
+  hashSync: (string, salt, iterations = 10000, keyLength = 64, digest = "sha512") => {
+    const derivedKey = pbkdf2Sync(string, salt, iterations, keyLength, digest);
+    return derivedKey.toString("hex");
   },
 
-  compareSync: (string, hashedString, salt) => {
-    const hash = cryptor.hashSync(string, salt);
+  compareSync: (
+    string,
+    hashedString,
+    salt,
+    iterations = 10000,
+    keyLength = 64,
+    digest = "sha512"
+  ) => {
+    const hash = cryptor.hashSync(string, salt, iterations, keyLength, digest);
     return hash === hashedString;
-  }
+  },
 };
 
 export default cryptor;
